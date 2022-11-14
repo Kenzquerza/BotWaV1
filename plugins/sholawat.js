1d48e28e4a0ff5e0c56d738f40b80036c7652071
Kenzquerza <ffraf9885@gmail.com>
2022-11-14 21:24:42 +0800

    KenZ XD

---
+++ plugins/sholawat.js
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+    let info = fs.readFileSync('./mp3/sholawat.opus')
+
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+
+conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://youtube.com/channel/UCFXzDd79rF07KwFeZbCFAzw",
+    mediaType: 2,
+    description: "https://youtube.com/channel/UCFXzDd79rF07KwFeZbCFAzw", 
+    title: 'KenZ XD?',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sig  }}})
+}
+handler.customPrefix = /^(sholawat)$/i
+handler.command = new RegExp
+
+export default handler
